8cc3d6f69fdd2a82097c8ed926da18f1dcd2cf83
shavrau <rhian.souza@fatec.sp.gov.br>
2024-03-11 12:16:35 -0300

    Aula 1103 - Validação de dominios e construtores

--- StockApp/StockApp.Domain/Entities/Category.cs
+++ StockApp/StockApp.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StockApp.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -7,11 +8,38 @@ using System.Threading.Tasks;
 
 namespace StockApp.Domain.Entities
 {
+    #region Atributos
     public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        #endregion
+    #region Construtores
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            id = Id;
+            ValidateDomain(name);
+        }
 
         public ICollection<Product> Products { get; set; }
+        #endregion
+    #region Validações
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+                "Invalid name, name is required");
+
+            DomainExceptionValidation.When(name.Length < 3, 
+                "Invalid name, too short, minimum 3 characters.");
+
+            Name = name;
+        }
+        #endregion
     }
 }
